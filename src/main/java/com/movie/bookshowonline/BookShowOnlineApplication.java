d5113683c874076a06ac93201a7f4e14d2bb8cf2
bharath <bharath110996@gmail.com>
2021-01-09 15:56:52 +0530

    Update BookShowOnlineApplication.java
    Main comments

--- src/main/java/com/movie/bookshowonline/BookShowOnlineApplication.java
+++ src/main/java/com/movie/bookshowonline/BookShowOnlineApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Main spring boot application
 public class BookShowOnlineApplication {
 
 	public static void main(String[] args) {
